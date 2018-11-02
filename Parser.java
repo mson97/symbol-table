2d01e2c656e0cfa275a892560cae9526769f2260
Minhee Son <mhson@ucdavis.edu>
2018-11-01 22:49:57 -0700

    Add files via upload
    Upload Symbol Table and Parser

---
+++ Parser.java
@@ -0,0 +1,230 @@
+package com.company;
+
+/* *** This file is given as part of the programming assignment. *** */
+
+public class Parser {
+
+    // tok is global to all these parsing methods;
+    // scan just calls the scanner's scan method and saves the result in tok
+    private Token tok; // the current token
+    private SymbolTable symbolTable;
+    private Token variable; // temp variable for symbols
+    // private int scope;
+
+    private void scan() {
+        tok = scanner.scan();
+    }
+
+    private Scan scanner;
+
+    Parser(Scan scanner) {
+        this.scanner = scanner;
+        symbolTable = new SymbolTable(); // initialize empty symbol table
+        scan();
+        program();
+        if (tok.kind != TK.EOF)
+            parse_error("junk after logical end of program");
+        symbolTable = null;
+    }
+
+    private void program() {
+        block();
+    }
+
+    private void block() {
+        declaration_list();
+        statement_list();
+    }
+
+    private void declaration_list() {
+        // below checks whether tok is in first set of declaration.
+        // here, that's easy since there's only one token kind in the set.
+        // in other places, though, there might be more.
+        // so, you might want to write a general function to handle that.
+        while (is(TK.DECLARE)) {
+            declaration();
+        }
+    }
+
+    private void declaration() {
+        mustbe(TK.DECLARE);
+        // save copy of curr tok
+        variable = this.tok;
+        mustbe(TK.ID);
+        // add variable to symbol table
+        symbolTable.declare(variable);
+        while (is(TK.COMMA)) {
+            scan();
+            // save copy
+            variable = this.tok;
+            mustbe(TK.ID);
+            // add variable to symbol table
+            symbolTable.declare(variable);
+        }
+    }
+
+    // check tok is in FS_(statement) = {'~', id, '<', '!', '['}
+    private boolean f_stmt() {
+        return (((is(TK.TILDA) || is(TK.ID)) || (is(TK.DO) || is(TK.PRINT))) || is(TK.IF));
+    }
+
+    private void statement_list() {
+        while (f_stmt()) {
+            statement();
+        }
+    }
+
+    private void statement() {
+        if (f_asmt()) {
+            assignment();
+        } else if (is(TK.PRINT)) {
+            print();
+        } else if (is(TK.DO)) {
+            e_do();
+        } else if (is(TK.IF)) {
+            e_if();
+        } else { // incorrect token type
+            parse_error("Error in statement");
+        }
+    }
+
+    // check tok is in FS_(assignment) = {'~', id}
+    private boolean f_asmt() {
+        return (is(TK.TILDA) || is(TK.ID));
+    }
+
+    private void assignment() {
+        ref_id();
+        // check if variable is in symbol table
+        symbolTable.assign(variable);
+        mustbe(TK.ASSIGN);
+        expr();
+    }
+
+    private void print() {
+        mustbe(TK.PRINT);
+        expr();
+    }
+
+    private void e_do() {
+        mustbe(TK.DO);
+        guarded_command();
+        mustbe(TK.ENDDO);
+    }
+
+    private void e_if() {
+        mustbe(TK.IF);
+        guarded_command();
+        while (is(TK.ELSEIF)) {
+            scan();
+            guarded_command();
+        }
+        if (is(TK.ELSE)) {
+            scan();
+            // start of nested block, enter new scope
+            symbolTable.enter_scope();
+            block();
+            // end of nested block, exit new scope
+            symbolTable.exit_scope();
+        }
+        mustbe(TK.ENDIF);
+    }
+
+    enum Scope {GLOBAL, CURRENT, NONE, GIVEN}
+
+    private void ref_id() {
+        int scope = -1;
+        // Assume Scope: none
+        Scope type = Scope.NONE;
+        if (is(TK.TILDA)) {
+            scan();
+            if (is(TK.NUM)) {
+                // Scope: given lvl
+                type = Scope.GIVEN;
+                scope = Integer.parseInt(tok.string);
+                scan();
+            } else type = Scope.GLOBAL; // Scope: global lvl
+        }
+        variable = this.tok;
+        mustbe(TK.ID);
+        // check variable is in symbol table at scope (if given)
+        symbolTable.assign(variable, type, scope);
+    }
+
+    private void expr() {
+        term();
+        while (f_addop()) {
+            scan();
+            term();
+        }
+    }
+
+    private void guarded_command() {
+        expr();
+        mustbe(TK.THEN);
+        // start of nested block, enter new scope
+        symbolTable.enter_scope();
+        block();
+        // end of nested block, exit new scope
+        symbolTable.exit_scope();
+    }
+
+    private void term() {
+        factor();
+        while (f_multop()) {
+            scan();
+            factor();
+        }
+    }
+
+    // check tok is in FS_(factor) = {'(', '~', id, number}
+    private void factor() {
+        if (is(TK.LPAREN)) {
+            scan();
+            expr();
+            mustbe(TK.RPAREN);
+        } else if (f_rfid()) {
+            ref_id();
+        } else if (is(TK.NUM)) {
+            scan();
+        } else {
+            parse_error("Error in factor");
+        }
+    }
+
+    // check tok is in FS_(ref_id) = {'~', id}
+    private boolean f_rfid() {
+        return (is(TK.TILDA) || is(TK.ID));
+    }
+
+    // check tok is in FS_(addop) = {'+', '-'}
+    private boolean f_addop() {
+        return (is(TK.PLUS) || is(TK.MINUS));
+    }
+
+    // check tok is in FS_(multop) = {'*', '/'}
+    private boolean f_multop() {
+        return (is(TK.TIMES) || is(TK.DIVIDE));
+    }
+
+    // is current token what we want?
+    private boolean is(TK tk) {
+        return tk == tok.kind;
+    }
+
+    // ensure current token is tk and skip over it.
+    private void mustbe(TK tk) {
+        if (tok.kind != tk) {
+            System.err.println("mustbe: want " + tk + ", got " +
+                    tok);
+            parse_error("missing token (mustbe)");
+        }
+        scan();
+    }
+
+    private void parse_error(String msg) {
+        System.err.println("can't parse: line "
+                + tok.lineNumber + " " + msg);
+        System.exit(1);
+    }
+}
---
+++ SymbolTable.java
@@ -0,0 +1,121 @@
+package com.company;
+
+import java.util.HashSet;
+import java.util.Stack;
+
+import static com.company.Parser.Scope;
+
+public class SymbolTable {
+
+    private Stack<HashSet<String>> table; // symbol table is stack: scope of hashsets: variables
+    private int curr_scope;
+
+    //create stack with empty hash set
+    public SymbolTable() {
+        table = new Stack<>();
+        table.push(new HashSet<>());
+        curr_scope = 0;
+    }
+
+    // remove last scope from symbol table
+    public void exit_scope() {
+        table.pop();
+        curr_scope--;
+    }
+
+    // create new scope in symbol table
+    public void enter_scope() {
+        table.push(new HashSet<>());
+        curr_scope++;
+    }
+
+    // checking if variable already exists in scope and adding to symbol table
+    public void declare(Token id) {
+        if (table.elementAt(curr_scope).contains(id.string)) {
+            System.err.println("redeclaration of variable " + id.string);
+            // ignore redeclaration
+        } else {
+            table.elementAt(curr_scope).add(id.string);
+        }
+    }
+
+    // recursively checking symbol table for variable (inside out)
+    public void assign(Token id) {
+        if (!find(id)) {
+            symbol_error(id);
+        }
+    }
+
+    // check symbol table for variable when (possibly) given scope
+    public boolean assign(Token id, Scope type, int scope) {
+        //no scope was given, check recursively
+        if (type == Scope.NONE) {
+            if (!find(id)) {
+                symbol_error(id);
+                return false;
+            }
+        }
+        // scope was given, check according to type of scope
+        else {
+            return locate(id, type, scope);
+        }
+        return false;
+    }
+
+    private boolean locate(Token id, Scope type, int scope) {
+        // search at: global lvl
+        if (type == Scope.GLOBAL) {
+            if (table.elementAt(0).contains(id.string)) {
+                return true;
+            } else {
+                symbol_error(id, type, scope);
+            }
+        } // search at: current lvl
+        else if (type == Scope.CURRENT) {
+            if (table.elementAt(curr_scope).contains(id.string)) {
+                return true;
+            } else {
+                symbol_error(id, type, scope);
+            }
+        } // search at: given (numerical) lvl
+        else {
+            // scope lvl: invalid, or variable not in scope
+            if ((curr_scope - scope < 0) ||
+                    !table.elementAt(curr_scope - scope).contains(id.string)) {
+                symbol_error(id, type, scope);
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void symbol_error(Token id) {
+        System.err.println(id.string + " is an undeclared variable on line " + id.lineNumber);
+        System.exit(1);
+    }
+
+    private void symbol_error(Token id, Scope type, int scope) {
+        if (type == Scope.GLOBAL) {
+            System.err.println("no such variable ~" + id.string + " on line " + id.lineNumber);
+        } else {
+            System.err.println("no such variable ~" + scope + id.string + " on line " + id.lineNumber);
+        }
+        System.exit(1);
+    }
+
+    private boolean find(Token id) {
+        return _find(id, curr_scope);
+    }
+
+    private boolean _find(Token id, int scope) {
+        if (table.elementAt(scope).contains(id.string)) {
+            return true;
+        } else if (scope <= 0) {
+            return false;
+        } else {
+            --scope;
+            return _find(id, scope);
+        }
+    }
+}
